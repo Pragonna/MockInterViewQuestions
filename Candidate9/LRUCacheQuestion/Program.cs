6eeef112e2bf98df87f37721f488ea5e03fddf6d
salihcantekin <salihcantekin@gmail.com>
2024-08-02 17:02:03 +0100

    Add project files.

---
+++ Candidate9/LRUCacheQuestion/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Running;
+
+var cache = new LRUCache(2);
+
+cache.Put(1, 1);                        // Adds the key 1 with value 1 to the cache
+cache.Put(2, 2);                        // Adds the key 2 with value 2 to the cache
+Console.WriteLine(cache.Get(1));        // returns 1
+cache.Put(3, 3);                        // evicts key 2
+Console.WriteLine(cache.Get(2));        // returns -1 (not found)
+cache.Put(4, 4);                        // evicts key 1
+Console.WriteLine(cache.Get(1));        // returns -1 (not found)
+Console.WriteLine(cache.Get(3));        // returns 3
+Console.WriteLine(cache.Get(4));        // returns 4
+
+
+class LRUCache(int capacity)
+{
+    public void Put(int key, int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Get(int key)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Candidate9/LRUCacheQuestion/Solutions/SimpleSolution.cs
@@ -0,0 +1,47 @@
+﻿namespace LRUCacheQuestion.Solutions;
+public class LRUCache
+{
+    private readonly int capacity;
+    private readonly Dictionary<int, int> cache;
+    private readonly List<int> usageOrder;
+
+    public LRUCache(int capacity)
+    {
+        this.capacity = capacity;
+        cache = new Dictionary<int, int>();
+        usageOrder = new List<int>();
+    }
+
+    public int Get(int key)
+    {
+        if (!cache.ContainsKey(key))
+            return -1;
+        
+        usageOrder.Remove(key);             // remove the key from the usage order
+        usageOrder.Add(key);                // add the key to the end of the usage order
+
+        return cache[key];
+    }
+
+    public void Put(int key, int value)
+    {
+        if (cache.ContainsKey(key))
+        {
+            cache[key] = value;             // update the value of the key
+            usageOrder.Remove(key);         // remove the key from the usage order
+            usageOrder.Add(key);            // add the key to the end of the usage order
+        }
+        else
+        {
+            if (cache.Count == capacity)    // reached capacity
+            {
+                int lruKey = usageOrder[0]; // get the least recently used key
+                usageOrder.RemoveAt(0);     // remove the least recently used key
+                cache.Remove(lruKey);       // remove the least recently used key from the cache
+            }
+
+            cache[key] = value;             // add the new key to the cache
+            usageOrder.Add(key);            // add the new key to the usage order
+        }
+    }
+}
